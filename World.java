a22e5295d967f9f79c8a9d22219aa66237504e4b
atomheartM <kimhyotae1026@gmail.com>
2026-05-13 11:03:18 +0900

    five

--- World.java
+++ World.java
@@ -2,4 +2,7 @@ public class World {
     public static void run() {
         System.out.println("202417139");
     }
+    public static void run2() {
+    System.out.println("hello world");
+    }
 }
